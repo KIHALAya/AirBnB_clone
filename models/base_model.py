bd7106ead3d64672bdc34c73759122c4c49a3d05
ElmourikiYassine <yassineelmouriki@gmail.com>
2023-12-09 00:25:26 -0800

    task_4 (base_model)

--- models/base_model.py
+++ models/base_model.py
@@ -23,11 +23,19 @@ class BaseModel:
     to_dict(self): returns a dictionary containing
                 all keys/values of __dict__ of the instance.
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ __init__ - set up the initial state of an object."""
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        setattr(self, key, datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def save(self):
         """ save - updates the public instance attribute
